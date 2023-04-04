106dbcb9dc2507037849ee53c4af8e3dd7d07233
NuZard84 <210130107019@gecg28.ac.in>
2023-04-04 18:58:49 +0530

    commit

--- components/billBoard.tsx
+++ components/billBoard.tsx
@@ -21,7 +21,7 @@ const BillBoard = () => {
       ></video>
       <button
         onClick={handleMute}
-        className="absolute top-[91%] ml-2 p-1 opacity-50 text-[12px] md:text-[16px] lg:text-[20px] md:ml-3 lg:ml-5 md:p-2 lg:p-2 rounded-md bg-white g-white hover:opacity-30 transition duration-300"
+        className="absolute flex flex-row items-center top-[91%] ml-2 p-1 opacity-50 text-[12px] md:text-[16px] lg:text-[20px] md:ml-3 lg:ml-5 md:p-2 lg:p-2 rounded-md bg-white g-white hover:opacity-30 transition duration-300"
       >
         {Mute ? <VscMute /> : <VscUnmute />}
       </button>
@@ -29,9 +29,14 @@ const BillBoard = () => {
         <p className="h-full text-white text-1x1 md:text-5xl w-[50%] lg:text-6xl font-bold drop-shadow-xl">
           {data?.title}
         </p>
-        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[60%] drop-shadow-xl">
+        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[55%] drop-shadow-xl">
           {data?.description}
         </p>
+        <div>
+          <button className="flex flex-row items-center gap-3 mt-3 bg-white lg:mt-5 md:mt-4">
+            More Info
+          </button>
+        </div>
       </div>
     </div>
   );
