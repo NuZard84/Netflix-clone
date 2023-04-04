82efde2796937d4b9a5be588d830c0df2c657336
NuZard84 <210130107019@gecg28.ac.in>
2023-04-05 01:37:15 +0530

    commit

--- components/billBoard.tsx
+++ components/billBoard.tsx
@@ -22,11 +22,11 @@ const BillBoard = () => {
       ></video>
       <button
         onClick={handleMute}
-        className="absolute flex flex-row items-center top-[93%] right-0 mr-1 p-1 opacity-50 text-[6px] md:text-[16px] lg:text-[20px] md:mr-3 lg:mr-5 md:p-2 lg:p-2 rounded-md bg-white g-white hover:opacity-30 transition duration-300"
+        className="absolute flex flex-row items-center top-[93%] right-0 mr-1 p-1 opacity-50 text-[7px] md:text-[16px] lg:text-[20px] md:mr-3 lg:mr-5 md:p-2 lg:p-2 rounded-md bg-white g-white hover:opacity-30 transition duration-300"
       >
         {Mute ? <VscMute /> : <VscUnmute />}
       </button>
-      <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+      <div className="absolute top-[60%] md:top-[45%] ml-4 md:ml-16">
         <p className="h-full text-white text-1x1 md:text-5xl w-[50%] lg:text-6xl font-bold drop-shadow-xl">
           {data?.title}
         </p>
@@ -35,7 +35,7 @@ const BillBoard = () => {
         </p>
         <div className="flex flex-row items-center gap-3 mt-2 md:mt-4">
           <button className="flex flex-row items-center w-auto px-2 py-1 text-xs font-semibold text-white transition bg-white rounded-md bg-opacity-30 md:py-2 md:px-4 lg:text-lg hover:bg-opacity-20">
-            <AiOutlineInfoCircle className="mr-1" />More Info
+            <AiOutlineInfoCircle className="mr-1" size={24} />More Info
           </button>
         </div>
       </div>
