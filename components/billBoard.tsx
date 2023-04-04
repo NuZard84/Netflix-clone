dbd12919c3e34b912752691f26b43117d9d07b17
NuZard84 <210130107019@gecg28.ac.in>
2023-04-05 01:43:30 +0530

    commit

--- components/billBoard.tsx
+++ components/billBoard.tsx
@@ -26,16 +26,16 @@ const BillBoard = () => {
       >
         {Mute ? <VscMute /> : <VscUnmute />}
       </button>
-      <div className="absolute top-[60%] md:top-[45%] ml-4 md:ml-16">
+      <div className="absolute top-[55%] lg:top-[62%] md:top-[45%] ml-4 md:ml-16">
         <p className="h-full text-white text-1x1 md:text-5xl w-[50%] lg:text-6xl font-bold drop-shadow-xl">
           {data?.title}
         </p>
-        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[60%] drop-shadow-xl">
+        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[75%] md:w-[69%] lg:w-[65%] drop-shadow-xl">
           {data?.description}
         </p>
         <div className="flex flex-row items-center gap-3 mt-2 md:mt-4">
           <button className="flex flex-row items-center w-auto px-2 py-1 text-xs font-semibold text-white transition bg-white rounded-md bg-opacity-30 md:py-2 md:px-4 lg:text-lg hover:bg-opacity-20">
-            <AiOutlineInfoCircle className="mr-1" size={24} />More Info
+            <AiOutlineInfoCircle className="mr-1 sm:text-xs"/>More Info
           </button>
         </div>
       </div>
